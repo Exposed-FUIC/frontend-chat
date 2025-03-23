e51d30ba226e8f7f106026165afa67e4b4c139fd
Josh Perry <joshpmperry@gmail.com>
2025-03-23 22:36:05 +0700

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 // UI imports
 import Header from "@/components/Header";
 import SearchBar from "@/components/SearchBar";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
