58b0b8d7a2b99c0cfd3d567aa6ed8a98325c8908
Josh Perry <joshpmperry@gmail.com>
2025-03-25 18:32:33 +0700

    Remove commented-out Header component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Header from "@/components/Header";
 import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[radial-gradient(circle,_gray-800_10%,_white_90%)]">
-      {/* <div className="">
-        <Header/>
-      </div> */}
-      
       <div className="flex h-dvh items-center justify-center">
         <SearchBar/>
       </div>
