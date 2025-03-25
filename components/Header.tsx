063975dcb15bbe477d231e5d3a91564c200800fe
pattapon-son <pattapon.son@student.mahidol.edu>
2025-03-25 17:46:13 +0700

    Added Buttons to SearchBar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button'
 
 function Header() {
   return (
-    <div className='flex items-center justify-between h-[70px] border shadow-xs px-5'>
+    <div className='flex w-full items-center justify-between h-[70px] absolute border shadow-xs px-5'>
         <div>
             Logo?
         </div>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Input } from "./ui/input";
-import { Search } from "lucide-react";
+import { Mic, Search, Wand } from "lucide-react";
+
 
 function SearchBar() {
   return (
-    <div className="flex mt-10 items-center justify-center">
-      <div className="relative w-[500px]">
+    <div className="flex mt-5 items-center justify-center">
+      <div className="relative w-[700px]">
         <Input
           type="text"
           className="h-14 w-full pr-12 pl-5 rounded-full focus:shadow focus:outline-none"
@@ -13,6 +14,14 @@ function SearchBar() {
         />
         <Search 
           className="absolute right-4 top-1/2 transform -translate-y-1/2 text-slate-400"
+          size={24}
+        />
+        <Mic 
+          className="absolute right-17 top-1/2 transform -translate-y-1/2 text-slate-400"
+          size={24}
+        />
+        <Wand
+          className="absolute right-11 top-1/2 transform -translate-y-1/2 text-slate-400"
           size={24}
         />
       </div>
